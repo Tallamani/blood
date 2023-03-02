fdb557915f7d9fa203b6c380fd04e86b28a7fa65
manikanta <kantamani296@gmail.com>
2023-03-02 22:46:19 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,231 @@
+const data=[
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"B+"
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"O+"
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+    },
+    {
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"B+"
+    },
+    {
+      "id": 6,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+    },
+    {
+      "id": 7,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"B+"
+      
+    },
+    {
+      "id": 8,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A-"
+      
+    },
+    {
+      "id": 9,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"B-"
+      
+    },
+    {
+      "id": 10,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 11,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 12,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 13,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 14,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 15,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+
+    {
+      "id": 16,
+      "name": "Leanne Graham",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+    },
+    {
+      "id": 17,
+      "name": "Ervin Howell",
+      "username": "Antonette",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"B+"
+    },
+    {
+      "id": 18,
+      "name": "Clementine Bauch",
+      "username": "Samantha",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"O+"
+    },
+    {
+      "id": 19,
+      "name": "Patricia Lebsack",
+      "username": "Karianne",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+    },
+    {
+      "id": 20,
+      "name": "Chelsey Dietrich",
+      "username": "Kamren",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"B+"
+    },
+    {
+      "id": 21,
+      "name": "Mrs. Dennis Schulist",
+      "username": "Leopoldo_Corkery",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+    },
+    {
+      "id": 22,
+      "name": "Kurtis Weissnat",
+      "username": "Elwyn.Skiles",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"B+"
+      
+    },
+    {
+      "id": 23,
+      "name": "Nicholas Runolfsdottir V",
+      "username": "Maxime_Nienow",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A-"
+      
+    },
+    {
+      "id": 24,
+      "name": "Glenna Reichert",
+      "username": "Delphine",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"B-"
+      
+    },
+    {
+      "id": 25,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 26,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 27,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 28,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 29,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+    {
+      "id": 30,
+      "name": "Clementina DuBuque",
+      "username": "Moriah.Stanton",
+      "phone": "1-770-736-8031 x56442",
+      "bloodGroup":"A+"
+      
+    },
+  ]
+
+  export default data;
